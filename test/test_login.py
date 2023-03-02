e23e08dbb2d4149c2cfc29d83c4a91ab5099ad75
dhara purohit <Iamqa@1993d>
2023-03-02 12:03:11 +0530

    Added login title

--- test/test_login.py
+++ test/test_login.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from assertpy import assert_that
+from selenium.webdriver.common.by import By
+
+
 class TestLoginUI:
     def test_title(self):
         driver=webdriver.Chrome()
@@ -9,3 +12,11 @@ class TestLoginUI:
         actual_title=driver.title
         assert actual_title == "OrangeHRM"
         assert_that("OrangeHRM").is_equal_to(actual_title)
+    def test_header(self):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.implicitly_wait(20)
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+        actual_header = driver.find_element(By.XPATH,"//h5[text()='Login']").text
+        assert actual_header == "Login"
+        assert_that("Login").is_equal_to(actual_header)
